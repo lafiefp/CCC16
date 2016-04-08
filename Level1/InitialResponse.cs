8e9ebeb0387fab5454711d04f851eb6bd09e5c24
Franz <franz.profelt@gmail.com>
2016-04-08 14:54:12 +0200

    addes response classes

---
+++ Level1/InitialResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Level1
+{
+    class InitialResponse : ResponseBase
+    {
+        public InitialResponse(float xmin, float xmax, float ymin, float ymax)
+            : base(3, new int[]{ 4, 1, 1 })
+        {
+            this.xmin = xmin;
+            this.xmax = xmax;
+            this.ymin = ymin;
+            this.ymax = ymax;
+        }
+
+        public float xmin;
+        public float xmax;
+        public float ymin;
+        public float ymax;
+    }
+}
