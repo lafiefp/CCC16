450eeb52e68085ef55d8208682236a003ea7a68c
Tom <Tom@TOM-PC>
2016-04-08 15:01:58 +0200

    commit

---
+++ Level1/StatusResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Level1
+{
+    class StatusResponse : ResponseBase
+    {
+        public StatusResponse()
+            : base(1, new int[]{ 9 })
+        {
+
+        }
+    }
+}
---
+++ Level1/TickResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Level1
+{
+    class TickResponse : ResponseBase
+    {
+        public enum SuccessState 
+        {
+            NoSuccess,
+            Success
+        }
+
+        public float Time;
+        public SuccessState State;
+
+        public TickResponse()
+            : base(2, new int[]{1, 1})
+        {}
+    }
+}
