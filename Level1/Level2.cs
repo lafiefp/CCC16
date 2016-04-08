42115d85920c49d2ad9b5b297b4bef80853a410c
Tom <Tom@TOM-PC>
2016-04-08 17:10:39 +0200

    pre merge

---
+++ Level1/Level2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Level1.Commands;
+
+namespace Level1
+{
+    class Level2
+    {
+        public static void Execute()
+        {
+            var comm = new Communicator();
+            var settings = comm.GetInitial2();
+
+
+
+            var cmdTick = new TickCommand();
+            if (!comm.ExecuteCommand(new ThrottleCommand(0, 0.6f)).Processed)
+            {
+                Console.WriteLine("Throttle not accepted!");
+                return;
+            }
+
+            TickResponse tick = new TickResponse();
+            var status = comm.ExecuteCommand(new StatusCommand(0));
+            while (!tick.Success)
+            {
+                Console.WriteLine(comm.ExecuteCommand(new StatusCommand(0)));
+                tick = comm.ExecuteCommand(cmdTick);
+            }
+
+            Console.WriteLine("Finished within " + tick.Time);
+        }
+    }
+}
--- Level1/Program.cs
+++ Level1/Program.cs
@@ -12,29 +12,9 @@ namespace Level1
         {
             Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
 
-            TickResponse tick = new TickResponse();
             try
             {
-                var comm = new Communicator();
-                var settings = comm.GetInitial();
-
-                var cmdTick = new TickCommand();
-
-
-                if (!comm.ExecuteCommand(new ThrottleCommand(0, 0.6f)).Processed)
-                {
-                    Console.WriteLine("Throttle not accepted!");
-                    return;
-                }
-
-                var status = comm.ExecuteCommand(new StatusCommand(0));
-                while (!tick.Success)
-                {
-                    Console.WriteLine(comm.ExecuteCommand(new StatusCommand(0)));
-                    tick = comm.ExecuteCommand(cmdTick);
-                }
-
-                Console.WriteLine("Finished within " + tick.Time);
+                Level1.Execute();
             }
             catch (ParseException e)
             {
