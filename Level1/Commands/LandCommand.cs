000e983bf8c0298f64b50e84756c7fcc264b401a
Franz <franz.profelt@gmail.com>
2016-04-08 17:19:03 +0200

    implemented land

---
+++ Level1/Commands/LandCommand.cs
@@ -0,0 +1,18 @@
+﻿using Level1.Responses;
+namespace Level1.Commands
+{
+    internal class LandCommand : CommandBase<LandResponse>
+    {
+        private readonly int _id;
+
+        public LandCommand(int id)
+        {
+            _id = id;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("LAND {0}\n", _id); // $"LAND {_id}\n";
+        }
+    }
+}
